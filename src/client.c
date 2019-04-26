d570062713254f1034fa7a797ae8640d383af09b
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-04-26 09:07:37 +0530

    Completed

--- src/client.c
+++ src/client.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 #define PORT 12345
 
 int main() {
@@ -27,18 +29,82 @@ int main() {
     else {
         printf("connected\n");
     }
-    char buffer[50] = "";
+    char buffer[10000] = "";
+    char fileName[30];
     while(1) {
         bzero(buffer, sizeof(buffer));
         printf("\n$\t:\t");
         scanf("%[^\n]%*c" , buffer);
         write(sockFD , buffer, strlen(buffer));
         printf("\nMessage Sent\t:\t%s\n" , buffer);
-        if(strcmp(buffer ,"QUIT") == 0 ) {
-            break;
+        if ( strlen(buffer) > 7) {
+            int len = strlen(buffer) - 5;
+            strncpy(fileName , buffer + 5 , len);
+            fileName[len] = '\0';
         }
         read(sockFD , buffer , sizeof(buffer));
-        printf("\nMessage Received\t:\t%s\n" , buffer);
+        if(strcmp(buffer ,"\nConnection Termination\n") == 0 ) {
+            printf("\nConnection Termination\n");
+            break;
+        }
+        if( strcmp(buffer , "\n225 	Data connection open; no transfer in progress.\n") == 0) {
+            printf("%s" , buffer);
+
+            printf("\nFile Sending In Process\n");
+            sleep(2);
+            FILE *fPtr;
+            fPtr = fopen(fileName, "r"); 
+            if (fPtr == NULL) { 
+                printf("Cannot open file\t:\t%s \n", fileName); 
+            }
+            else {
+                char ch;
+                bzero(buffer, sizeof(buffer));
+                int i = 0;
+                while(1) {
+                    while ( ( (ch = fgetc(fPtr)) != EOF ) && ( strlen(buffer) - 1 != sizeof(buffer)) ) {
+                        buffer[i] = ch;
+                        i++;
+                    }
+                    buffer[i] = '\0';
+                    if ( ch == EOF) {
+                        write(sockFD , buffer, strlen(buffer));
+                        break;
+                    }
+                    if ( strlen(buffer) == sizeof(buffer) ) {
+                        write(sockFD , buffer, strlen(buffer));
+                        bzero(buffer, sizeof(buffer));
+                        i = 0;
+                    }
+                }
+                printf("\nFile Sent Successfully\n");
+                fclose(fPtr);
+            }
+        }
+        else if ( strcmp(buffer , "\n226 	Data connection open; no transfer in progress.\n") == 0) {
+            printf("%s" , buffer);
+            FILE *fPtr;
+            fPtr = fopen(fileName, "w"); 
+            if (fPtr == NULL) { 
+                printf("Cannot open file\t:\t%s \n", fileName);  
+            }
+            else {
+                bzero(buffer, sizeof(buffer));
+                while (1) {
+                    read(sockFD , buffer , sizeof(buffer));
+                    fputs(buffer , fPtr);
+                    if ( strlen(buffer) != sizeof(buffer) ) {
+                        break;
+                    }
+                    bzero(buffer, sizeof(buffer));
+                }
+                printf("\nFile Received Successfully\n");
+                fclose(fPtr);
+            }
+        }
+        else {
+            printf("%s" , buffer);
+        }
     }
     close(sockFD);
 
